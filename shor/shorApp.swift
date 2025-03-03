6b2268ff3a221c851269a0636fe0db375dc91cc1
Nouraibn <Noura_alnassar@hotmail.com>
2025-03-03 14:28:09 +0300

    Initial Commit

---
+++ shor/shorApp.swift
@@ -0,0 +1,17 @@
+//
+//  shorApp.swift
+//  shor
+//
+//  Created by Noura on 03/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct shorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
